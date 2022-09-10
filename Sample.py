8399a81b65deeea63c30a1ac2af7d5a7fe3fcb85
nagendrakalyan97 <testsuccess3@gmail.com>
2022-09-10 21:11:07 +0530

    Generalised the method to give sum of number

--- Sample.py
+++ Sample.py
@@ -1,8 +1,15 @@
 import random
+def SumOfNumbers(Input):
+    count = 0
+    while(Input>0):
+        count += Input%10
+        Input = int(Input/10)
+    return count
 
-Input = int(random.randint(999,99999))
-count = 0
-while(Input!=0):
-    count += Input%10
-    Input = int(Input/10)
-print("Count is: ",count) 
+Input = int(random.randint(9,999999999))
+print("Given input number is: ", Input)
+result = SumOfNumbers(Input)
+while(result > 9):
+    result = SumOfNumbers(result)
+else:
+    print("Count of numbers is", result)
